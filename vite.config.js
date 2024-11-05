2660802db8053474d16507b1321d15bbaaa65adc
Sofiia <sofiiavoitkova@gmail.com>
2024-11-05 21:12:02 +0100

    chore[WEB-9]: add path aliases for src directories

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,11 @@ export default defineConfig({
     alias: {
       '@assets': path.resolve(__dirname, '/src/assets'),
       '@components': path.resolve(__dirname, '/src/components'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@routing': path.resolve(__dirname, 'src/routing'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
     },
   },
   css: {
