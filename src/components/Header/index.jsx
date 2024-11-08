67dd72005e5e8d8417395bbfd49fb28d7698ba81
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-11-08 21:44:54 +0100

    feat[WEB-3]: add header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,31 @@
 import styles from './header.module.scss'
+import logo from '/src/assets/header/logo.svg'
+import subLogo from '/src/assets/header/subText.svg'
+import langIcon from '/src/assets/header/language.svg'
 
 export default function Header() {
   return (
     <header className={styles.container}>
-      <div>Title</div>
-      <div>Content</div>
+      <div className={styles.mainDiv}>
+        <div className={styles.logoContainer}>
+          <img src={logo} alt="Logo" />
+          <img src={subLogo} alt="Sub Logo" />
+        </div>
+        <div className={styles.linksContainer}>
+          <a className={styles.link} href="#">
+            Prototype
+          </a>
+          <a className={styles.link} href="#">
+            Discover
+          </a>
+          <a className={styles.link} href="#">
+            About Us
+          </a>
+        </div>
+        <div className={styles.lang}>
+          <img src={langIcon} alt="language" />
+        </div>
+      </div>
     </header>
   )
 }
