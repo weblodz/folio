a139c83fcb286fa42d18ca929706b0bc4e5ba61d
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-11-11 21:28:59 +0100

    feat[WEB-2]: add sections' components to the home page layout, add positioning styles

---
+++ src/pages/Home/components/DiscoverSection/index.jsx
@@ -0,0 +1,11 @@
+import styles from './discover.module.scss'
+
+function DiscoverSection() {
+  return (
+    <div className={styles.container}>
+      <h1>header</h1>
+    </div>
+  )
+}
+
+export default DiscoverSection
