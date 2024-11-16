4b391e3ab76c3bd924c774f76da156331f6a2ddb
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-11-16 11:51:40 +0100

    fix[WEB-#]: add missing import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import Default from '@components/Default'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Home from './pages/Home/index'
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className={styles.main_container}>
       <Header />
+      <Default />
       <Footer />
       <Routes>
         <Route path="/" element={<Home />} />
