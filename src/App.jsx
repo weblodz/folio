09cf29faa20f2afa7564e4e86da7fcd4d87a7017
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-11-16 11:37:53 +0100

    fix[WEB-3]: reorganize imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
-import Default from '@components/Default'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import styles from './app.module.scss'
 import Home from './pages/Home/index'
+import styles from './app.module.scss'
 
 function App() {
   return (
     <div className={styles.main_container}>
       <Header />
-      <Default />
       <Footer />
       <Routes>
         <Route path="/" element={<Home />} />
