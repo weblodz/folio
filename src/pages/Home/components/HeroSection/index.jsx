2582568ce3afe1138ba3c6bf0e14b4cbc7497d0c
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-11-21 14:20:23 +0100

    feat[WEB-5]: add all elements, add style to the main header element

--- src/pages/Home/components/HeroSection/index.jsx
+++ src/pages/Home/components/HeroSection/index.jsx
@@ -3,7 +3,24 @@ import styles from './hero.module.scss'
 function HeroSection() {
   return (
     <div className={styles.container}>
-      <h1>HEADER</h1>
+      <div className={styles.contentContainer}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.mainText}>
+            Mobility with <br /> an Emotional Connection
+          </h1>
+          <div className={styles.fullMovieContainer}>
+            <a className={styles.fullMovieLink}>
+              <span className={styles.fullMovieIcon}></span>
+              <span className={styles.fullMovieText}>Play a full movie</span>
+            </a>
+          </div>
+        </div>
+        <div className={styles.newsletterContainer}></div>
+        <div className={styles.backgroundContainer}></div>
+        <div className={styles.playButtonContainer}>
+          <span></span>
+        </div>
+      </div>
     </div>
   )
 }
