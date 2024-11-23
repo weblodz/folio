08299e58522ec0b6518f9b40facbd60b15149409
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-11-23 17:31:13 +0100

    feat[WEB-5]: add play/pause button component

--- src/pages/Home/components/HeroSection/index.jsx
+++ src/pages/Home/components/HeroSection/index.jsx
@@ -1,17 +1,19 @@
 import styles from './hero.module.scss'
 import NewsletterLink from '@pages/Home/components/HeroSection/components/newsletterLink/index.jsx'
 import MainText from '@pages/Home/components/HeroSection/components/mainText'
+import PlayButton from '@pages/Home/components/HeroSection/components/playButton/index.jsx'
+import { useState } from 'react'
 
 function HeroSection() {
+  const [isBgPlaying, setBgPlaying] = useState(false)
+
   return (
     <div className={styles.container}>
       <div className={styles.contentContainer}>
         <MainText />
         <NewsletterLink />
         <div className={styles.backgroundContainer}></div>
-        <div className={styles.playButtonContainer}>
-          <span></span>
-        </div>
+        <PlayButton isBgPlaying={isBgPlaying} setBgPlaying={setBgPlaying} />
       </div>
     </div>
   )
