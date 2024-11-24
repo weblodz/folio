346db3638b306cd0b50a941a781f2c413d856669
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-11-24 18:16:32 +0100

    feat[WEB-5]: add functionality to the show-full-movie link

--- src/pages/Home/components/HeroSection/index.jsx
+++ src/pages/Home/components/HeroSection/index.jsx
@@ -2,19 +2,22 @@ import styles from './hero.module.scss'
 import NewsletterLink from '@pages/Home/components/HeroSection/components/newsletterLink/index.jsx'
 import MainText from '@pages/Home/components/HeroSection/components/mainText'
 import PlayButton from '@pages/Home/components/HeroSection/components/playButton/index.jsx'
+import { ModalPlayer } from '@pages/Home/components/HeroSection/components/modalPlayer/index.jsx'
 import { useState } from 'react'
 import { VideoBG } from '@pages/Home/components/HeroSection/components/videoBG/index.jsx'
 
 function HeroSection() {
   const [isBgPlaying, setBgPlaying] = useState(true)
+  const [isModalPlayerShown, setModalPlayerShown] = useState(false)
 
   return (
     <div className={styles.container}>
       <div className={styles.contentContainer}>
-        <MainText />
+        <MainText setModalPlayerShown={setModalPlayerShown} />
         <NewsletterLink />
         <VideoBG isBgPlaying={isBgPlaying} />
         <PlayButton isBgPlaying={isBgPlaying} setBgPlaying={setBgPlaying} />
+        {isModalPlayerShown && <ModalPlayer setModalPlayerShown={setModalPlayerShown} />}
       </div>
     </div>
   )
