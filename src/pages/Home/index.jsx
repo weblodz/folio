17e5c8a9d5594ac6b9cf932719df9a4af0ab3c18
Mykola Horbenko <mykola.horbenko22@gmail.com>
2024-11-28 16:43:39 +0100

    fix[WEB-5]: remove a redundant piece of code

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
-      <HeroSection isModalPlayerShown={isModalPlayerShown} setModalPlayerShown={setModalPlayerShown} />
+      <HeroSection setModalPlayerShown={setModalPlayerShown} />
       <PrototypeSection />
       <UpdatesSection />
       <DiscoverSection />
