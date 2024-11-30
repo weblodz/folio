dcb5da3064c9b5675403afdea043dce5c328fb0f
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-11-30 14:32:41 +0100

    feat[web-14]: add modal container

--- src/components/Default/index.jsx
+++ src/components/Default/index.jsx
@@ -1,8 +1,23 @@
 import styles from './default.module.scss'
+import Modal from '@components/Modals/VideoModals/Modal'
+import { useState } from 'react'
 
 export default function Default() {
+  const [openModal, setOpenModal] = useState(false)
+
   return (
     <div className={styles.container}>
+      <div className={styles.link}>
+        <a className={styles.modal} href='"https://www.youtube.com/embed/4Oj_fenobDw?enablejsapi=1"' onClick={() => {
+          setOpenModal(true)
+        }}>
+          Open Modal Button
+        </a>
+        <div className={styles.result}>
+          {openModal && <Modal />}
+        </div>
+
+      </div>
       <div>Title</div>
       <div>Content</div>
     </div>
---
+++ src/components/Modals/VideoModals/Modal.jsx
@@ -0,0 +1,12 @@
+import styles from './modal.module.scss'
+
+function Modal() {
+  return (
+    <div className={styles.modal_background}>
+      Modal
+    </div>
+
+  )
+}
+
+export default Modal
