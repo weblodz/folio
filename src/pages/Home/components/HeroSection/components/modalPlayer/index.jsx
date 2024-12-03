d154fb56e79d6adbddb02cb9892f685f964fa1f0
Mykola Horbenko <mykola.horbenko22@gmail.com>
2024-12-03 10:19:05 +0100

    fix[WEB-5]: fix the modal player window. Implement it using React Portals tool

--- src/pages/Home/components/HeroSection/components/modalPlayer/index.jsx
+++ src/pages/Home/components/HeroSection/components/modalPlayer/index.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom'
 import { CloseButton } from './components/closeButton/index.jsx'
 import styles from './modalPlayer.module.scss'
 
 export function ModalPlayer({ setModalPlayerShown }) {
-  return (
+  return ReactDOM.createPortal(
     <div
       className={styles.container}
       onClick={() => {
@@ -17,14 +18,14 @@ export function ModalPlayer({ setModalPlayerShown }) {
             width="1280"
             height="720"
             src="https://www.youtube.com/embed/4Oj_fenobDw?enablejsapi=1"
-            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen=""
           ></iframe>
         </div>
       </div>
       <CloseButton setModalPlayerShown={setModalPlayerShown} />
-    </div>
+    </div>,
+    document.getElementById('modal-root'),
   )
 }
 
--- src/pages/Home/components/HeroSection/index.jsx
+++ src/pages/Home/components/HeroSection/index.jsx
@@ -4,10 +4,12 @@ import MainText from '@pages/Home/components/HeroSection/components/mainText'
 import PlayButton from '@pages/Home/components/HeroSection/components/playButton/index.jsx'
 import { useState } from 'react'
 import { VideoBG } from '@pages/Home/components/HeroSection/components/videoBG/index.jsx'
+import { ModalPlayer } from '@pages/Home/components/HeroSection/components/modalPlayer/index.jsx'
 import styles from './hero.module.scss'
 
-function HeroSection({ setModalPlayerShown }) {
+function HeroSection() {
   const [isBgPlaying, setBgPlaying] = useState(true)
+  const [isModalPlayerShown, setModalPlayerShown] = useState(false)
 
   return (
     <div className={styles.container}>
@@ -16,6 +18,7 @@ function HeroSection({ setModalPlayerShown }) {
         <NewsletterLink />
         <VideoBG isBgPlaying={isBgPlaying} />
         <PlayButton isBgPlaying={isBgPlaying} setBgPlaying={setBgPlaying} />
+        {isModalPlayerShown && <ModalPlayer setModalPlayerShown={setModalPlayerShown} />}
       </div>
     </div>
   )
