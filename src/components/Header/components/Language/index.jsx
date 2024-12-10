f4563265006a60f5aff94e6e6af9c48d91ae33ac
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-12-10 20:08:34 +0100

    feat[WEB-17]: add language buttons and dropdown toggle functionality

--- src/components/Header/components/Language/index.jsx
+++ src/components/Header/components/Language/index.jsx
@@ -1,12 +1,35 @@
+import { useState } from 'react'
 import langIcon from '/src/assets/header/language.svg'
 import styles from './language.module.scss'
 
 function Language() {
+
+  const [isDropdownVisible, setDropdownVisible] = useState(false)
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!isDropdownVisible)
+  }
+
   return (
     <div className={styles.icon}>
-      <button className={styles.lang} type="button">
+      <button className={styles.lang} type="button" onClick={toggleDropdown}>
         <img src={langIcon} alt="language" />
       </button>
+
+      {isDropdownVisible && (
+      <ul className={styles.langButtons}>
+        <li className={styles.item}>
+          <a className={styles.link} href="#">
+            English
+          </a>
+        </li>
+        <li className={styles.item}>
+          <a className={styles.link} href="#">
+            Polish
+          </a>
+        </li>
+      </ul>
+      )}
     </div>
   )
 }
