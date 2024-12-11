c5c214d889557e1f2d2fa0effa3963a4e5ed8c44
Sofiia <sofiiavoitkova@gmail.com>
2024-12-12 00:15:19 +0100

    feat[web-16]: add responsive design for logo and enhance it for vlog text

--- src/components/Footer/Components/Logo/index.jsx
+++ src/components/Footer/Components/Logo/index.jsx
@@ -5,8 +5,8 @@ import styles from './logo.module.scss'
 function Logo() {
   return (
     <div className={styles.logoContainer}>
-      <img src={logoTitle} alt="Logo Title" />
-      <img src={logoSubTitle} alt="Logo Subtitle" />
+      <img src={logoTitle} className={styles.logoTitle} alt="Logo Title" />
+      <img src={logoSubTitle} className={styles.logoSubTitle} alt="Logo Subtitle" />
     </div>
   )
 }
