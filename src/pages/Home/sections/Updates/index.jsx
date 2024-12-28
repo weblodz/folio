7b91ca7f705e9bc14272e745954f2c08435f9997
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-12-28 20:07:28 +0100

    feat[WEB-19]: update Latest Updates section layout and styles

--- src/pages/Home/sections/Updates/index.jsx
+++ src/pages/Home/sections/Updates/index.jsx
@@ -1,7 +1,15 @@
+import image from '@assets/image.webp'
 import styles from './updates.module.scss'
 
 function UpdatesSection() {
-  return <div className={styles.container}>text</div>
+  return <div className={styles.container}>
+    <div className={styles.section}>
+      <div className={styles.name}>
+        <h1>Latest Updates</h1>
+        <img className={styles.image} src={image} alt="Qualcomm’s annual Holiday Marketplace"></img>
+      </div>
+    </div>
+  </div>
 }
 
 export default UpdatesSection
