04d893d908f4948e2908dcf3276cd8decd53373f
Sofiia <sofiiavoitkova@gmail.com>
2024-12-30 01:12:11 +0100

    chore[web-20]: set temporarily the link to # until the route is ready

--- src/pages/Home/sections/Discover/components/Content/index.jsx
+++ src/pages/Home/sections/Discover/components/Content/index.jsx
@@ -9,7 +9,7 @@ function Content() {
     <div className={styles.container}>
       <h3 className={styles.subtitle}>Future Voice vol.6 AFEELA: The Entertainment Destination</h3>
       <p className={styles.description}>Future Voice vol.6 AFEELA: The Entertainment Destination</p>
-      <Link to="/stories" className={styles.link}>
+      <Link to="#" className={styles.link}>
         Read more
       </Link>
     </div>
