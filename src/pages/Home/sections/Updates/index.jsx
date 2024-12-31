728d708075d2d10e146dae800864edc3bf1b5321
GarnaA <143414379+GarnaA@users.noreply.github.com>
2024-12-31 17:23:19 +0100

    feat[WEB-19]: add Latest Updates section (#20)
    * feat[WEB-19]: update Latest Updates section layout and styles
    
    * feat[WEB-19]: add image grid layout to Latest Updates section
    
    * feat[WEB-19]: add Latest Updates section

--- src/pages/Home/sections/Updates/index.jsx
+++ src/pages/Home/sections/Updates/index.jsx
@@ -1,7 +1,68 @@
+import { useState } from 'react'
+import cls from 'classnames'
+import { Link } from 'react-router-dom'
+import image from '@assets/latestUpdates/image.webp'
+import image1 from '@assets/latestUpdates/image1.webp'
+import image2 from '@assets/latestUpdates/image2.webp'
+import image3 from '@assets/latestUpdates/image3.webp'
+import image4 from '@assets/latestUpdates/image4.webp'
 import styles from './updates.module.scss'
 
-function UpdatesSection() {
-  return <div className={styles.container}>text</div>
+function Updates() {
+  const [isHovered, setIsHovered] = useState(false)
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.section}>
+        <h1 className={styles.name}>Latest Updates</h1>
+        <div className={styles.news_wrapper}>
+          <div className={styles.column}>
+            <div className={styles.main_image_wrapper}>
+              <img className={cls(styles.image, {[styles.hovered]: isHovered})} src={image} />
+            </div>
+          </div>
+          <div className={styles.column}>
+            <div
+              className={styles.article}
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              <div className={styles.date}>12.20.2024</div>
+              <div className={styles.description}>
+                <p>The AFEELA prototype 2024 was</p>
+                <p>featured at Qualcomm’s annual</p>
+                <p>Holiday Marketplace.</p>
+              </div>
+              <Link to="/news" className={styles.read_more}>
+                Read more
+              </Link>
+            </div>
+          </div>
+          <div className={styles.column}>
+            <div className={styles.news_grid}>
+              <div className={styles.grid_column}>
+                <div className={styles.grid_item}>
+                  <img src={image1} />
+                </div>
+                <div className={styles.grid_item}>
+                  <img src={image2} />
+                </div>
+              </div>
+
+              <div className={styles.grid_column}>
+                <div className={styles.grid_item}>
+                  <img src={image3} />
+                </div>
+                <div className={styles.grid_item}>
+                  <img src={image4} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default UpdatesSection
+export default Updates
