4c0a8e2e0a1b7aca7149edf5d3269e87d94a07e2
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-12-31 17:04:42 +0100

    feat[WEB-19]: add Latest Updates section

--- src/pages/Home/sections/Updates/index.jsx
+++ src/pages/Home/sections/Updates/index.jsx
@@ -1,59 +1,62 @@
+import { useState } from 'react'
+import cls from 'classnames'
 import { Link } from 'react-router-dom'
-import image from '@assets/image.webp'
-import image1 from '@assets/image1.webp'
-import image2 from '@assets/image2.webp'
-import image3 from '@assets/image3.webp'
-import image4 from '@assets/image4.webp'
+import image from '@assets/latestUpdates/image.webp'
+import image1 from '@assets/latestUpdates/image1.webp'
+import image2 from '@assets/latestUpdates/image2.webp'
+import image3 from '@assets/latestUpdates/image3.webp'
+import image4 from '@assets/latestUpdates/image4.webp'
 import styles from './updates.module.scss'
 
-function UpdatesSection() {
+function Updates() {
+  const [isHovered, setIsHovered] = useState(false)
+
   return (
     <div className={styles.container}>
       <div className={styles.section}>
-        {/* Header */}
         <h1 className={styles.name}>Latest Updates</h1>
-
-        {/* Main Content */}
-        <div className={styles.mainContent}>
-          {/* Left Column with Image */}
-          <img
-            className={styles.image}
-            src={image}
-            alt="Qualcomm’s annual Holiday Marketplace"
-          />
-
-          {/* Right Column with Text */}
-          <div className={styles.content}>
-            <div className={styles.date}>12.20.2024</div>
-            <div className={styles.description}>
-              <p>The AFEELA prototype 2024 was</p>
-              <p>featured at Qualcomm’s annual</p>
-              <p>Holiday Marketplace.</p>
-            </div>
-            {/* Read more link */}
-            <Link to="/read-more" className={styles.readMore}>
-              Read more
-            </Link>
-          </div>
-        </div>
-
-        {/* Info Grid Section */}
-        <div className={styles.infoGrid}>
-          <div className={styles.gridColumn}>
-            <div className={styles.gridItem}>
-              <img src={image1} alt="Event 1" />
-            </div>
-            <div className={styles.gridItem}>
-              <img src={image2} alt="Event 2" />
+        <div className={styles.news_wrapper}>
+          <div className={styles.column}>
+            <div className={styles.main_image_wrapper}>
+              <img className={cls(styles.image, {[styles.hovered]: isHovered})} src={image} />
             </div>
           </div>
-
-          <div className={styles.gridColumn}>
-            <div className={styles.gridItem}>
-              <img src={image3} alt="Event 3" />
+          <div className={styles.column}>
+            <div
+              className={styles.article}
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              <div className={styles.date}>12.20.2024</div>
+              <div className={styles.description}>
+                <p>The AFEELA prototype 2024 was</p>
+                <p>featured at Qualcomm’s annual</p>
+                <p>Holiday Marketplace.</p>
+              </div>
+              <Link to="/news" className={styles.read_more}>
+                Read more
+              </Link>
             </div>
-            <div className={styles.gridItem}>
-              <img src={image4} alt="Event 4" />
+          </div>
+          <div className={styles.column}>
+            <div className={styles.news_grid}>
+              <div className={styles.grid_column}>
+                <div className={styles.grid_item}>
+                  <img src={image1} />
+                </div>
+                <div className={styles.grid_item}>
+                  <img src={image2} />
+                </div>
+              </div>
+
+              <div className={styles.grid_column}>
+                <div className={styles.grid_item}>
+                  <img src={image3} />
+                </div>
+                <div className={styles.grid_item}>
+                  <img src={image4} />
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -62,4 +65,4 @@ function UpdatesSection() {
   )
 }
 
-export default UpdatesSection
+export default Updates
