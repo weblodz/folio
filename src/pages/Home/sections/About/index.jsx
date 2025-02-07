3e3f68e8d7a00fb107a07e1eb05c2cc069a730ce
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-02-07 16:22:05 +0100

    feat[WEB-21]: move background video component to the general components folder

--- src/pages/Home/sections/About/index.jsx
+++ src/pages/Home/sections/About/index.jsx
@@ -2,7 +2,7 @@ import Content from '@pages/Home/sections/About/Components/Content/index.jsx'
 import TitleAbout from '@pages/Home/sections/About/Components/Title/index.jsx'
 import MoreLink from '@pages/Home/sections/About/Components/Link/index.jsx'
 import useVisibilityObserver from '@hooks/useVisibilityObserver.js'
-import VideoBG from '@pages/Home/sections/Hero/components/VideoBG/index.jsx'
+import VideoBG from '@components/VideoBG/index.jsx'
 import styles from './about.module.scss'
 
 function About() {
