051735b338f65a7e818b9aa24ab53ab5a7f27bca
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-02-10 16:34:15 +0100

    feat[WEB-26]: add Email Input Component

---
+++ src/components/UiKit/FormInputs/EmailInput/index.jsx
@@ -0,0 +1,28 @@
+import T from 'prop-types'
+import styles from './emailinput.module.scss'
+
+function EmailInput({ value="", onChange, onBlur=null, placeholder, isValid=true, errorMessage }) {
+  const emailStyle = isValid ? `${styles.emailInput}` : `${styles.emailInput} ${styles.invalidEmailInput}`
+  const isInputFocused = value === "" ? `${styles.labelText}` : `${styles.labelText} ${styles.focusedLabelText}`
+
+  return (
+    <div className={styles.componentContainer}>
+      <div className={styles.inputContainer} tabIndex={0}>
+        <input className={emailStyle} value={value} type="text" onChange={onChange} onBlur={onBlur}/>
+        <label className={isInputFocused}>{placeholder}</label>
+      </div>
+      {isValid ? null : <span className={styles.errorMessage}>{errorMessage}</span>}
+    </div>
+  )
+}
+
+export default EmailInput
+
+EmailInput.propTypes = {
+  value: T.string.isRequired,
+  onChange: T.func.isRequired,
+  placeholder: T.string.isRequired,
+  isValid: T.bool.isRequired,
+  errorMessage: T.string.isRequired,
+  onBlur: T.func
+}
