bd00d0867e5c048bc97662972a2cf1409e1921ca
Sofiia Voitkova <sofiiavoitkova@gmail.com>
2025-02-10 19:31:53 +0100

    feat[web-28]: add namespaces and implement translations for Hero section

--- src/services/i18n.js
+++ src/services/i18n.js
@@ -1,8 +1,13 @@
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
-import enTranslations from './translations/en.json'
-import plTranslations from  './translations/pl.json'
+
+import enCommon from './translations/locales/en/nsCommon.json'
+import plCommon from './translations/locales/pl/nsCommon.json'
+
+import enHero from './translations/locales/en/nsHero.json'
+import plHero from './translations/locales/pl/nsHero.json'
+
 
 i18n
   .use(LanguageDetector)
@@ -10,14 +15,18 @@ i18n
   .init({
     resources: {
       en: {
-        translation: enTranslations
+        nsCommon: enCommon,
+        nsHero: enHero,
       },
       pl: {
-        translation: plTranslations
-      }
+        nsCommon: plCommon,
+        nsHero: plHero,
+      },
     },
     lng: 'en',
     fallbackLng: 'en',
+    ns: ['nsCommon, nsHero'],
+    defaultNS: 'nsCommon',
     detection: {
       order: ["navigator", "localStorage"],
       caches: ["localStorage"]
