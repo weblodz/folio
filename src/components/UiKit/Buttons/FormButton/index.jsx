433c3fcc473af948926fdbb4b6ee9c6642dd3689
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-02-10 13:54:20 +0100

    feat[WEB-26]: add onClick and disables properties to the form button

--- src/components/UiKit/Buttons/FormButton/index.jsx
+++ src/components/UiKit/Buttons/FormButton/index.jsx
@@ -1,9 +1,10 @@
 import T from 'prop-types'
 import styles from './formbuttom.module.scss'
 
-function FormButton({ text, isActive }) {
+function FormButton({ text, isActive, onClick }) {
   return (
-    <button className={isActive ? styles.activeButton : styles.inactiveButton}>{text}</button>
+    <button className={isActive ? styles.activeButton : styles.inactiveButton}
+      onClick={() => {onClick()}} disabled={!isActive}>{text}</button>
   )
 }
 
@@ -12,4 +13,5 @@ export default FormButton
 FormButton.propTypes = {
   text: T.string.isRequired,
   isActive: T.bool.isRequired,
+  onClick: T.func.isRequired,
 }
