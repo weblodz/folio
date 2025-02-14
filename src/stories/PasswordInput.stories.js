2159c8feb8f2ed0b19c2af0f548cb38880ebc5d0
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-02-14 15:32:18 +0100

    feat[WEB-32]: add stories for Password Input

---
+++ src/stories/PasswordInput.stories.js
@@ -0,0 +1,30 @@
+import PasswordInput from '@components/UiKit/FormInputs/PasswordInput/index.jsx'
+
+export default {
+  title: 'PasswordInput',
+  component: PasswordInput,
+}
+
+export const EmptyValid = {
+  args: {
+    isValid: true,
+    errorMessage: "",
+    defaultValue: "",
+  }
+}
+
+export const withValidPassword = {
+  args: {
+    isValid: true,
+    errorMessage: "",
+    defaultValue: "ValidSecurePassword",
+  }
+}
+
+export const withInvalidPassword = {
+  args: {
+    isValid: false,
+    errorMessage: "Password is invalid",
+    defaultValue: "InvalidPassword",
+  }
+}
