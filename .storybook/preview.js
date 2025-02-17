a434e1bf138527a5cb3c8357a4c01ca1e835974b
GarnaA <143414379+GarnaA@users.noreply.github.com>
2025-02-17 15:48:50 +0100

    feat[web-29]: add ArrowRightButton to Storybook (#30)
    * fix[web-29]: improve props naming and add onClick handler
    
    * feat[web-29]: remove unnecessary hover styles and add isLightTheme prop
    
    * feat[web-29]: improve styles and add isLightTheme prop
    
    * feat[web-29]: improve styles and add isLightTheme prop
    
    * fix[web-29]: improve button styles
    
    * style[web-29]: remove unnecessary styling
    
    * feat[web-29]: add ArrowRightButton to Storybook
    
    * fix[web-29]: use predefined color variables
    
    * style[web-29]: clean up redundant comments
    
    * feat[web-29]: change isLightTheme on hover in Prototype section
    
    * feat[web-29]: update button  props and styling
    
    * style:[web-29]: improve button styling
    
    * fix[web-29]: remove inline styles and use classnames

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
 /** @type { import('@storybook/react').Preview } */
+import '../src/index.scss'
+
 const preview = {
   parameters: {
     controls: {
@@ -6,6 +8,13 @@ const preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    backgrounds: {
+      default: 'black',
+      values: [
+        { name: 'white', value: 'var(--common-color-bg-primary)' },
+        { name: 'black', value: 'var(--common-color-bg-inverse-primary)'}
+      ],
     },
   },
 }
