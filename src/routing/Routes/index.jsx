835de22d1b22fb650614f455461e314c73f018a7
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2025-02-17 20:32:03 +0100

    fix[web-30]: add PrivateRoute component for protected routes

--- src/routing/Routes/index.jsx
+++ src/routing/Routes/index.jsx
@@ -3,6 +3,7 @@ import Home from '@pages/Home'
 import SignIn from '@pages/Auth/SignIn'
 import SignUp from '@pages/Auth/SignUp'
 import Profile from '@pages/Auth/Profile'
+import PrivateRoute from '@routing/PrivateRoute'
 import routes from '../path'
 
 function AppRoutes() {
@@ -11,7 +12,9 @@ function AppRoutes() {
       <Route path={routes.withoutAuth.home} element={<Home />} />
       <Route path={routes.withoutAuth.signIn} element={<SignIn />} />
       <Route path={routes.withoutAuth.signUp} element={<SignUp />} />
-      <Route path={routes.withAuth.profile} element={<Profile />} />
+      <Route element={<PrivateRoute />}>
+        <Route path={routes.withAuth.profile} element={<Profile />} />
+      </Route>
     </Routes>
   )
 }
