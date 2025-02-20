2500857ac97bd0955b285515302cf2b75ef75fce
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-02-20 21:22:51 +0100

    feat[WEB-33]: add mobile version to Sign in page

---
+++ src/components/Auth/DataPolicy/index.jsx
@@ -0,0 +1,18 @@
+import styles from './datapolicy.module.scss'
+
+function DataPolicy() {
+  return (
+    <div className={styles.container}>
+      <span className={styles.text}>
+        By signing up with your social accounts, you agree to Sony Honda Mobility&apos;s
+        <span className={styles.text_link}>AFEELA Account Terms and Conditions</span>
+        and
+        <span className={styles.text_link}>Privacy Policy.</span>
+        You also agree that Sony Honda Mobility, its affiliates and service providers
+        send you newsletters, marketing materials and other information.
+      </span>
+    </div>
+  )
+}
+
+export default DataPolicy
