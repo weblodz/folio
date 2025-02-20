ede9875454a079040b3192637fda74dea69d11ec
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-02-20 21:38:02 +0100

    feat[WEB-33]: add functionality of redirection between sign in and sign up pages

--- src/pages/Auth/SignUp/index.jsx
+++ src/pages/Auth/SignUp/index.jsx
@@ -1,7 +1,40 @@
+import SocialMedias from '@components/Auth/SocialMedias/index.jsx'
+import DataPolicy from '@components/Auth/DataPolicy/index.jsx'
+import { useNavigate } from 'react-router-dom'
 import styles from './signup.module.scss'
 
-export default function SignUp() {
+function SignUp(){
+  const navigate = useNavigate()
+
   return (
-    <div className={styles.container}></div>
+    <div className={styles.container}>
+      <div className={styles.content_container}>
+        <div className={styles.title_container}>
+          Create an account
+        </div>
+        <div className={styles.main_content_container}>
+          <div className={styles.description}>
+            <span className={styles.description_title}>Register your sign in information</span>
+            Only residents of Europe can register
+          </div>
+          <div className={styles.input_container}>
+            <div className={styles.form_container}></div>
+            <div className={styles.social_medias_container}>
+              <div className={styles.social_medias_title}>
+                Alternatively, sign in with your social accounts.
+              </div>
+              <SocialMedias />
+            </div>
+            <DataPolicy />
+            <hr />
+            <button className={styles.sign_in_button} onClick={() => {navigate('/sign-in')}}>
+              Sign in
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
+
+export default SignUp
