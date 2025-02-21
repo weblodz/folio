21834f6239b8c9eef65cecc1b2f224c24a52b89a
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2025-02-21 08:11:26 +0100

    feat[web-34]: add name input

---
+++ src/components/UiKit/FormInputs/NameInput/index.jsx
@@ -0,0 +1,58 @@
+import { useState, forwardRef } from "react"
+import PropTypes from "prop-types"
+import styles from "./nameinput.module.scss"
+
+const validateName = (name) => {
+  if (!name.trim()) return "Enter your first name"
+
+  return ""
+}
+
+const NameInput = forwardRef(({ value, onChange, errorMessage, ...rest }, ref) => {
+  const [localError, setLocalError] = useState(errorMessage || "")
+  const [isTouched, setIsTouched] = useState(false)
+
+  const handleChange = (e) => {
+    const newValue = e.target.value
+
+    if (isTouched) setLocalError(validateName(newValue))
+    onChange(newValue)
+  }
+
+  const handleBlur = () => {
+    setIsTouched(true)
+    setLocalError(validateName(value))
+  }
+
+  const isValid = !localError
+
+  return (
+    <div className={styles.name_container}>
+      <div className={styles.name_input_container} tabIndex={0}>
+        <input
+          className={isValid ? styles.name_input : `${styles.name_input} ${styles.invalid_name_input}`}
+          type="text"
+          ref={ref}
+          value={value}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          {...rest}
+        />
+        <label className={value.trim() ? `${styles.label_text} ${styles.focused_label_text}` : styles.label_text}>
+          {"First name"}
+        </label>
+      </div>
+      {!isValid && <span className={styles.error_message}>{localError}</span>}
+    </div>
+  )
+})
+
+NameInput.displayName = "NameInput"
+
+export default NameInput
+
+NameInput.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  errorMessage: PropTypes.string,
+}
