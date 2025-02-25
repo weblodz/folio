fb966f286aaab9b674034d069d3f99a57ca63e63
Mykola Horbenko <133876909+MyHKo@users.noreply.github.com>
2025-02-25 14:41:42 +0100

    feat[WEB-33]: add sign in and sign up pages (was fixed, due to the redundant commits from previous branches) (#33)
    * feat[WEB-33]: add the Sign in Page
    
    * feat[WEB-33]: codestyle fixes
    
    * feat[WEB-33]: import personal data policies as a component into SignIn component
    
    * feat[WEB-33]: move social media link to a separate component
    
    * feat[WEB-33]: include social media links to Sign In component
    
    * feat[WEB-33]: rename icon-components for social media logos
    
    * feat[WEB-33]: add Sign Up page
    
    * feat[WEB-33]: fix styles in sign in page, add mixins
    
    * feat[web-30]: add sign-in, sign-up, and private profile routes (#31)
    
    * feat[web-30]: add sign-in, sign-up, and private profile routes
    
    * fix[web-30]: remove index and use shorter folder name
    
    * fix[web-30]: add PrivateRoute component for protected routes
    
    * fix[web-30]: add new line at the end of the files
    
    * feat[WEB-33]: move sign in page to a different directory
    
    * feat[WEB-33]: delete unesseccary files
    
    * feat[WEB-33]: add mobile version to sign up page
    
    * feat[WEB-33]: remove redundant files
    
    * feat[WEB-33]: add mobile version to Sign in page
    
    * feat[WEB-33]: fix fonts in sign up page
    
    * feat[WEB-33]: fix fonts in sign in page
    
    * feat[WEB-33]: add functionality of redirection between sign in and sign up pages
    
    * feat[WEB-33]: add functionality of scrolling to the top of the page when redirected to another page
    
    * feat[WEB-33]: resolve commits after the cherrypicking
    
    * feat[WEB-33]: codestyle fixes
    
    * feat[WEB-33]: fix nagivage path and move navigate function outside of jsx
    
    * feat[WEB-33]: change container names for more clear ones
    
    * feat[WEB-33]: add polish translation to the sign in page
    
    * feat[WEB-33]: add polish translation to Data Policy component
    
    * feat[WEB-33]: add polish translation to Sign Up page
    
    * feat[WEB-33]: move all auth namespaces to a single one
    
    * feat[WEB-33]: fix import statements
    
    * feat[WEB-33]: change basic functions to arrow functions
    
    * feat[WEB-33]: fix function calls
    
    ---------
    
    Co-authored-by: GarnaA <143414379+GarnaA@users.noreply.github.com>

--- src/services/i18n.js
+++ src/services/i18n.js
@@ -5,6 +5,9 @@ import enCommon from './translations/locales/en/nsCommon.json'
 import plCommon from './translations/locales/pl/nsCommon.json'
 import enHome from './translations/locales/en/nsHome.json'
 import plHome from './translations/locales/pl/nsHome.json'
+import enAuth from './translations/locales/en/nsAuth.json'
+import plAuth from './translations/locales/pl/nsAuth.json'
+
 
 i18n
   .use(LanguageDetector)
@@ -14,15 +17,17 @@ i18n
       en: {
         nsCommon: enCommon,
         nsHome: enHome,
+        nsAuth: enAuth
       },
       pl: {
         nsCommon: plCommon,
         nsHome: plHome,
+        nsAuth: plAuth
       },
     },
     lng: 'en',
     fallbackLng: 'en',
-    ns: ['nsCommon', 'nsHome'],
+    ns: ['nsCommon', 'nsHome', 'nsAuth'],
     defaultNS: 'nsCommon',
     detection: {
       order: ["navigator", "localStorage"],
