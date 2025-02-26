b51fcc9789a9b2311cda9a735e26f980ecb37da0
Sofiia Voitkova <sofiiavoitkova@gmail.com>
2025-02-26 13:01:31 +0100

    fix[web-25]: prevent scrollbar jump by dynamically applying margin

--- src/components/Header/components/HamburgerButton/index.jsx
+++ src/components/Header/components/HamburgerButton/index.jsx
@@ -1,8 +1,33 @@
 import PropTypes from "prop-types"
+import { useEffect } from "react"
 import cls from "classnames"
 import styles from "./hamburgerButton.module.scss"
 
 function HamburgerButton({ isOpen, onToggle }) {
+
+  useEffect(() => {
+    if (isOpen) {
+      const scrollBarWidth = window.innerWidth - document.documentElement.clientWidth
+
+      document.body.style.overflow = 'hidden'
+      document.documentElement.style.setProperty('--scrollbar-width', `${scrollBarWidth}px`)
+
+      const handleResize = () => {
+        if (window.innerWidth > 768) {
+          onToggle()
+        }
+      }
+
+      window.addEventListener('resize', handleResize)
+
+      return () => {
+        document.body.style.overflow = ''
+        document.documentElement.style.removeProperty('--scrollbar-width')
+        window.removeEventListener('resize', handleResize)
+      }
+    }
+  }, [isOpen, onToggle])
+
   return (
     <button className={cls(styles.button, { [styles.toggled]: isOpen })} onClick={onToggle}>
       <div className={styles.bar}></div>
--- src/components/Header/components/Menu/index.jsx
+++ src/components/Header/components/Menu/index.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import SOCIAL_LINKS from '@components/Footer/Components/Socials/socialLinks'
 import MenuList from './components/MenuList'
@@ -6,23 +5,6 @@ import Newsletter from './components/Newsletter'
 import styles from './menu.module.scss'
 
 export default function Menu({ onClose }) {
-  useEffect(() => {
-    document.body.style.overflow = 'hidden'
-
-    const handleResize = () => {
-      if (window.innerWidth > 768) {
-        onClose()
-      }
-    }
-
-    window.addEventListener('resize', handleResize)
-
-    return () => {
-      document.body.style.overflow = ''
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [onClose])
-
   return (
     <div className={styles.container}>
       <MenuList onClose={onClose} />
