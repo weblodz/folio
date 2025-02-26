02fe54e07ceaef4539a21620e8399ec85f3e7b14
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-02-26 14:37:18 +0100

    feat[WEB-36]: slight fixes in Email Input component

--- src/components/UiKit/FormInputs/EmailInput/index.jsx
+++ src/components/UiKit/FormInputs/EmailInput/index.jsx
@@ -2,7 +2,7 @@ import { useState, forwardRef } from "react"
 import T from "prop-types"
 import styles from "./emailinput.module.scss"
 
-const EmailInput = forwardRef(({ isValid = true, errorMessage, defaultValue, ...rest }, ref) => {
+const EmailInput = forwardRef(({ isValid = true, errorMessage = "", defaultValue, ...rest }, ref) => {
   const [hasValue, setHasValue] = useState(defaultValue)
 
   const emailStyle = isValid
@@ -38,7 +38,7 @@ export default EmailInput
 
 EmailInput.propTypes = {
   isValid: T.bool.isRequired,
-  errorMessage: T.string.isRequired,
+  errorMessage: T.string,
   defaultValue: T.bool,
 }
 
