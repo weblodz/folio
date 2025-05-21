b81ff84d778265a9e97317b1d3320eb63e65d544
Andrii Zakharchenko <art.pruttov@gmail.com>
2025-05-21 13:01:32 +0300

    feat: add docker config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import { createHash } from 'crypto'
 import path from 'node:path'
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173,
+  },
+
   plugins: [react()],
   resolve: {
     alias: {
